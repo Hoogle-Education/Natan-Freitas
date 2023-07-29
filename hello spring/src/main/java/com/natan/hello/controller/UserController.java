a2a2b6d7f6694c9c0071086cc850818bb4f94fbf
hgrafa <hugorafell@gmail.com>
2023-07-29 17:03:18 -0300

    adding hello world spring project

---
+++ hello spring/src/main/java/com/natan/hello/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.natan.hello.controller;
+
+import com.natan.hello.model.User;
+import com.natan.hello.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    // requisicao = Verbo + Rota
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
